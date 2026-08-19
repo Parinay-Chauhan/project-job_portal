c7df2fd743e5f6bfcd2def8fbdc5318ab23ce4a9
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-08-19 23:34:46 +0530

    first commit

---
+++ db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from '../src/constant.js'
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        console.log(`MongoDB connect successfully on port ${PORT}`);
+
+    } catch (error) {
+        console.log("MongoDB connection error", error);
+
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+import connectDB from '../db/index.js'
+import { app } from './app.js'
+
+// require('dotenv').config({
+//     path: './.env'
+// })
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(` Server is running on port at ${process.env.PORT}`);
+
+        })
+    })
+    .catch((error) => {
+        console.log(`MondoDB connection fail !!!`, error);
+
+    })
