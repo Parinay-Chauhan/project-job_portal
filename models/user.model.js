04e77b0d085c3d393994d6826e647143c96e6370
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-08-21 22:22:20 +0530

    Add password hashing middleware to user schema before saving

--- models/user.model.js
+++ models/user.model.js
@@ -34,4 +34,10 @@ const userSchema = new Schema({
     { timestamps: true }
 )
 
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
 export const User = mongoose.model("User", userSchema)    
