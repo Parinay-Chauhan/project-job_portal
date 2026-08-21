e49f7acd26d0debd083410bec3558c1d7931beb9
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-08-21 22:40:37 +0530

    Add password hashing and JWT methods to user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema({
 
@@ -34,10 +36,16 @@ const userSchema = new Schema({
     { timestamps: true }
 )
 
-userSchema.pre("save", async function () {
-    if (!this.isModified("password")) return;
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
 
     this.password = await bcrypt.hash(this.password, 10);
+    next()
 });
 
-export const User = mongoose.model("User", userSchema)    
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+
+export const User = mongoose.model("User", userSchema)
