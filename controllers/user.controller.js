3d4a62d9ef870f498bc73495e2fa73e01c26fdc2
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-08-24 22:42:59 +0530

    Refactor user registration logic for improved readability and consistency

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { User } from "../models/user.model.js"
 
+
 const registerUser = asyncHandler(async (req, res) => {
 
     // +++++++++++++ Data Validation +++++++++++++
@@ -12,15 +13,14 @@ const registerUser = asyncHandler(async (req, res) => {
     // Check if all required fields are provided and not empty
 
     if ([username, email, password].some((field) => !field?.trim()
-    )
-    ) {
+    )) {
         throw new ApiError(400, "All fields are required")
     }
 
     // Check if the user already exists in the database
 
     const existedUser = await User.findOne({
-        $or: [{ email }, { username }],
+        $or: [{ username }, { email }],
 
     })
     if (existedUser) {
@@ -30,13 +30,13 @@ const registerUser = asyncHandler(async (req, res) => {
     // Create a new user in the database ( MongoDB will handle password hashing if you have set up pre-save middleware in the User model)
 
     const user = await User.create({
-        fullName,
+        // fullName,
         email,
         password,
         username,
     })
 
-   // Fetch the newly created user from the database and exclude the password field from the response     
+    // Fetch the newly created user from the database and exclude the password field from the response     
 
     const createdUser = await User.findById(user._id).select(
         "-password"
@@ -50,7 +50,7 @@ const registerUser = asyncHandler(async (req, res) => {
     // Send a success response with the created user data (excluding the password) and a success message
 
     res.status(201).json(
-    new ApiResponse(201, createdUser, "User registered successfully")
+        new ApiResponse(201, createdUser, "User registered successfully")
     )
 })
 
