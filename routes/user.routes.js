e1e5ee46da94d422d8ebf35b6b5f5540b24e85aa
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-08-24 22:42:11 +0530

    Fix import statement for user registration controller in user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,5 @@
-import express from 'express'
 import { Router } from 'express'
-import { registerUser } from '../controllers/user.controller'
+import { registerUser } from '../controllers/user.controller.js'
 
 const router = Router()
 
