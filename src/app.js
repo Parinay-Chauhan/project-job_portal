3ded7093b7d66e7d92322c4f1e906c5d73187367
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-08-25 19:54:05 +0530

    Remove unused user controller, database connection, error handling middleware, user model, and utility functions

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 
 //  ------------------- import routes ----------------------
 
-import userRouter from '../routes/user.routes.js'
+import userRouter from './routes/user.routes.js'
 
 //  --------------- routes declaration --------------------
 
