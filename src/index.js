bcbdf666d52b81368866002fc978bcf09f2ecaec
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-08-30 01:01:04 +0530

    Update server startup log message to include an icon

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ dotenv.config({
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
-            console.log(` Server is running on port at ${process.env.PORT}`);
+            console.log(`⚙️  Server is running on port at ${process.env.PORT}`);
 
         })
     })
