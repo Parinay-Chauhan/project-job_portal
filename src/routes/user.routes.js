64e4d71f9e87ffbe05d4adb4546a7d833cddeed7
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-01 20:40:10 +0530

    Add change-password route with JWT verification

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,7 @@ import {
   logoutUser,
   refreshAccessToken,
   getCurrentUser,
+  changePassword,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middleware/Auth.middleware.js";
 
@@ -17,5 +18,6 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/change-password").post(verifyJWT, changePassword);
 
 export default router;
