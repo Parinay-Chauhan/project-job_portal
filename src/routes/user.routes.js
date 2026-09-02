b69f00fe2f61912a00c4ef7a0638fafcfa607454
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-02 23:33:20 +0530

    Add test upload route and integrate updateUserAvatar for Cloudinary uploads

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,8 +7,10 @@ import {
   getCurrentUser,
   changePassword,
   updateAccountDetails,
+  updateUserAvatar,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middleware/Auth.middleware.js";
+import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router();
 
@@ -21,8 +23,13 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/change-password").post(verifyJWT, changePassword);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.post("/test-upload", upload.single("avatar"), (req, res) => {
+  res.status(200).json({
+    message: "File uploaded successfully",
+    file: req.file,
+  });
+});
 
-
-
+router.post("/test-cloudinary", upload.single("avatar"), updateUserAvatar);
 
 export default router;
