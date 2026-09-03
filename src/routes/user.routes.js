8989d0b6fe41ae97533348d7529568e833aec382
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-03 23:38:53 +0530

    Refactor user routes to clean up commented code and ensure proper handling of avatar updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,13 +23,18 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/change-password").post(verifyJWT, changePassword);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
-router.post("/test-upload", upload.single("avatar"), (req, res) => {
-  res.status(200).json({
-    message: "File uploaded successfully",
-    file: req.file,
-  });
-});
+// router.post("/test-upload", upload.single("avatar"), (req, res) => {
+//   res.status(200).json({
+//     message: "File uploaded successfully",
+//     file: req.file,
+//   });
+// });
 
-router.post("/test-cloudinary", upload.single("avatar"), updateUserAvatar);
+// router.post("/test-cloudinary", upload.single("avatar"), updateUserAvatar);
+
+router.patch("/update-avatar", verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+
+
 
 export default router;
