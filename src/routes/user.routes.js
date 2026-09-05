149d7a6cba7cedcd7df183e2088a7695328d3dd8
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-05 22:57:59 +0530

    Remove commented test routes for avatar upload in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,15 +23,6 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/change-password").post(verifyJWT, changePassword);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
-// router.post("/test-upload", upload.single("avatar"), (req, res) => {
-//   res.status(200).json({
-//     message: "File uploaded successfully",
-//     file: req.file,
-//   });
-// });
-
-// router.post("/test-cloudinary", upload.single("avatar"), updateUserAvatar);
-
 router.patch("/update-avatar", verifyJWT, upload.single("avatar"), updateUserAvatar);
 
 
