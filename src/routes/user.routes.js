0ff3623d3526d2d05a2041a087414cd882b462dc
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-07 22:46:39 +0530

    Refactor update-avatar route for improved readability

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -24,10 +24,9 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/change-password").post(verifyJWT, changePassword);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
-router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router.route("/delete-avatar").delete(verifyJWT, deleteUserAvatar);
 
 export default router;
-
-
-// router.patch("/update-avatar",verifyJWT,upload.single("avatar"),updateUserAvatar);
