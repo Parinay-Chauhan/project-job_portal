0e2806b38569d1398f9534e43e3cb936f19acb2c
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-07 22:47:05 +0530

    Refactor app.js for consistent formatting and improved readability

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
-import express from 'express'
-import cors  from "cors"
-import cookieParser from 'cookie-parser'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+const app = express();
 
-const app = express()
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
-
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))  // HTMl form se data aayega
-app.use(express.static("public"))  // file, folder, pdf, images ko server per store kerna
-app.use(cookieParser())
-
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // HTMl form se data aayega
+app.use(express.static("public")); // file, folder, pdf, images ko server per store kerna
+app.use(cookieParser());
 
 // --------- routes -----------------
 
-
 //  ------------------- import routes ----------------------
 
-import userRouter from './routes/user.routes.js'
+import userRouter from "./routes/user.routes.js";
+import candidateRouter from "./routes/candidate.routes.js";
 
 //  --------------- routes declaration --------------------
 
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/candidates", candidateRouter);
 
-app.use("/api/v1/users", userRouter)
-
-export { app }
+export { app };
