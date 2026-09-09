e769bde46217553c0477355a1e75821617cea943
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-09 19:16:44 +0530

    feat: Add education route to candidate routes for managing candidate education

--- src/routes/candidate.routes.js
+++ src/routes/candidate.routes.js
@@ -7,22 +7,22 @@ import {
   addExperience,
   updateExperience,
   deleteExperience,
+  addEducation,
+  // updateEducation,
+  // deleteEducation,
 } from "../controllers/candidate.controller.js";
 
 const router = Router();
 
-router
-  .route("/profile")
-  .post(verifyJWT, createCandidateProfile)
-  .get(verifyJWT, getCandidateProfile);
-
+router.route("/profile").post(verifyJWT, createCandidateProfile).get(verifyJWT, getCandidateProfile);
 router.route("/profile").patch(verifyJWT, updateCandidateProfile);
 
 router.route("/experience").post(verifyJWT, addExperience);
-
 router.patch("/experience/:experienceId", verifyJWT, updateExperience);
-
 router.delete("/experience/:experienceId", verifyJWT, deleteExperience);
 
+router.route("/education").post(verifyJWT, addEducation);
+// router.patch("/education/:educationId", verifyJWT, updateEducation);
+// router.delete("/education/:educationId", verifyJWT, deleteEducation);
 
 export default router;
