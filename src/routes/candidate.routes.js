cd9d06a4f7fdf4ce586c712dad9cbfa464e2fc82
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-10 22:40:02 +0530

    feat: Refactor resume route to improve readability and add deleteResume functionality

--- src/routes/candidate.routes.js
+++ src/routes/candidate.routes.js
@@ -11,11 +11,15 @@ import {
   updateEducation,
   deleteEducation,
   uploadAndUpdateResume,
+  deleteResume,
 } from "../controllers/candidate.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 const router = Router();
 
-router.route("/profile").post(verifyJWT, createCandidateProfile).get(verifyJWT, getCandidateProfile);
+router
+  .route("/profile")
+  .post(verifyJWT, createCandidateProfile)
+  .get(verifyJWT, getCandidateProfile);
 router.route("/profile").patch(verifyJWT, updateCandidateProfile);
 
 router.route("/experience").post(verifyJWT, addExperience);
@@ -26,7 +30,10 @@ router.route("/education").post(verifyJWT, addEducation);
 router.patch("/education/:educationId", verifyJWT, updateEducation);
 router.delete("/education/:educationId", verifyJWT, deleteEducation);
 
-router.route("/resume").post(verifyJWT, upload.single("resume"), uploadAndUpdateResume);
-
+// router.route("/resume").post(verifyJWT, upload.single("resume"), uploadAndUpdateResume);
+router
+  .route("/resume")
+  .post(verifyJWT, upload.single("resume"), uploadAndUpdateResume)
+  .delete(verifyJWT, deleteResume);
 
 export default router;
