594741ecee104a692f5526e435772148db627220
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-11 01:10:32 +0530

    refactor: Simplify error handling and improve readability in JWT verification middleware

--- src/middleware/Auth.middleware.js
+++ src/middleware/Auth.middleware.js
@@ -4,27 +4,46 @@ import { asyncHandler } from "../utils/AsyncHandler.js";
 import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
 
-    try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-
-        if (!token) {
-            throw new ApiError(401, "Unauthorize request")
-        }
-
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-
-        if (!user) {
-            throw new ApiError(401, "Invalid access Token")
-        }
-
-        req.user = user;
-        next()
-
-    } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token")
+    if (!token) {
+      throw new ApiError(401, "Unauthorize request");
     }
 
-})
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken");
+
+    if (!user) {
+      throw new ApiError(401, "Invalid access Token");
+    }
+
+    req.user = user;
+    next()
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
+
+
+export const authorizeRoles = (...allowedRoles) => {
+  return (req, _, next) => {
+
+    if (!req.user) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      throw new ApiError(
+        403,
+        `Access denied. Role '${req.user.role}' is not allowed to access this resource.`,
+      );
+    }
+
+    next();
+  };
+};
