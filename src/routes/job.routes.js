73b6c524c975fe652292cbeedf034b34bbde865c
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-12 15:21:42 +0530

    feat: Add public job retrieval route and update recruiter job management routes

--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -1,12 +1,30 @@
 import { Router } from "express";
 import { verifyJWT, authorizeRoles } from "../middleware/Auth.middleware.js";
-import { postJob } from "../controllers/job.controller.js";
+import {
+  postJob,
+  getAllJobs,
+  getMyPostedJobs,
+//   getJobById,
+//   updateJob,
+//   deleteJob,
+} from "../controllers/job.controller.js";
 
 const router = Router();
 
-// Recruiter-only route for creating jobs
+// Public Routes
+router.route("/").get(getAllJobs);
+router.route("/get/:id").get(getJobById); // public view single job
+
+// Recruiter Restricted Routes
+router.route("/").post(verifyJWT, authorizeRoles("recruiter"), postJob);
+
 router
-  .route("/")
-  .post(verifyJWT, authorizeRoles("recruiter"), postJob);
+  .route("/my-jobs")
+  .get(verifyJWT, authorizeRoles("recruiter"), getMyPostedJobs);
+
+// router
+//   .route("/:id")
+//   .patch(verifyJWT, authorizeRoles("recruiter"), updateJob)
+//   .delete(verifyJWT, authorizeRoles("recruiter"), deleteJob);
 
 export default router;
